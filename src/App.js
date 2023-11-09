b71debe0b7ec93d404dea1722386068e8d4c755a
Vinicius Pessoa <100362556+vspessoa0@users.noreply.github.com>
2023-11-09 11:35:33 -0300

    preparando ambiente

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import { Card } from './components'
+
+const App = () => {
+  return (
+    <div className='container'>
+      <Card />
+    </div>
+  )
+}
+
+export default App
